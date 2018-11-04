6051fcf0618a7f572c5419c59e571b905d221ae4
k0pernicus <antonin.carette@gmail.com>
2018-11-05 00:51:40 +0100

    Implemented a first version of IDT

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,29 @@
+#![no_std]
+
+#![feature(abi_x86_interrupt)]
 extern crate x86_64;
-use x86_64::structures::idt::InterruptDescriptorTable;
+use x86_64::structures::idt::{ExceptionStackFrame, InterruptDescriptorTable};
+
+#[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
+extern crate vga;
+use vga::buffer::BUF_WRITER;
+use core::fmt::Write;
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut ExceptionStackFrame) {
+    echo!(BUF_WRITER.lock(), "Breakpoint\n{:?}", stack_frame);
+}
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
 
 pub fn init_idt() {
-    let mut idt = InterruptDescriptorTable::new();
+    IDT.load();
 }
