fbdaccb02c8fbdba124832f7f358e313bc3a776a
k0pernicus <antonin.carette@gmail.com>
2019-01-09 19:56:21 +0100

    Export PICS in src/lib.rs, instead of src/handlers.rs

--- src/lib.rs
+++ src/lib.rs
@@ -14,9 +14,17 @@ extern crate vga;
 extern crate spin;
 
 extern crate intel_8259;
+use intel_8259::intel8259::Intel8259;
 
 mod handlers;
 use handlers::{breakpoint_handler, double_fault_handler};
+
+pub const MASTER_OFFSET: u8 = 32;
+pub const SLAVE_OFFSET: u8 = MASTER_OFFSET + 8;
+
+lazy_static! {
+    pub static ref PICS: spin::Mutex<Intel8259> = spin::Mutex::new(unsafe { Intel8259::new(MASTER_OFFSET, SLAVE_OFFSET) });
+}
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
