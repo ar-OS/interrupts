f9f52b726af27e3d3bec9931d84bb9d41a6d5b50
k0pernicus <antonin.carette@gmail.com>
2019-01-27 14:38:00 +0100

    Added explicit EOI in timer_interrupt_handler fn

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,6 +1,7 @@
 use x86_64::structures::idt::ExceptionStackFrame;
 use vga::buffer::BUF_WRITER;
 use core::fmt::Write;
+use {PICS, TIMER_INTERRUPT_ID};
 
 pub extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut ExceptionStackFrame, error_code: u64) {
     echo!(BUF_WRITER.lock(), "Double Fault\nError code: {}\n{:?}", error_code, stack_frame);
@@ -12,5 +13,6 @@ pub extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut ExceptionStac
 
 pub extern "x86-interrupt" fn timer_handler(stack_frame: &mut ExceptionStackFrame) {
     echo!(BUF_WRITER.lock(), ".");
+    unsafe { PICS.lock().send_end_interrupt(TIMER_INTERRUPT_ID) };
 }
 
