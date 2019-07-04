ae075410b773ca447f82e157be6eab5389ac8c87
k0pernicus <antonin.carette@gmail.com>
2019-07-04 19:48:34 +0200

    Usage of rust-fmt on handlers.rs

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,10 +1,18 @@
-use x86_64::structures::idt::ExceptionStackFrame;
-use vga::buffer::BUF_WRITER;
 use core::fmt::Write;
+use vga::buffer::BUF_WRITER;
+use x86_64::structures::idt::ExceptionStackFrame;
 use {PICS, TIMER_INTERRUPT_ID};
 
-pub extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut ExceptionStackFrame, error_code: u64) {
-    echo!(BUF_WRITER.lock(), "Double Fault\nError code: {}\n{:?}", error_code, stack_frame);
+pub extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: &mut ExceptionStackFrame,
+    error_code: u64,
+) {
+    echo!(
+        BUF_WRITER.lock(),
+        "Double Fault\nError code: {}\n{:?}",
+        error_code,
+        stack_frame
+    );
 }
 
 pub extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut ExceptionStackFrame) {
@@ -15,4 +23,3 @@ pub extern "x86-interrupt" fn timer_handler(stack_frame: &mut ExceptionStackFram
     echo!(BUF_WRITER.lock(), ".");
     unsafe { PICS.lock().send_end_interrupt(TIMER_INTERRUPT_ID) };
 }
-
